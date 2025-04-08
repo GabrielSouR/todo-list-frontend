fc31b952fe84b05c73d36cfc054ccfeabf8e3369
Gabriel <d2020025314@unifei.edu.br>
2025-04-08 19:57:47 -0300

    .

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,12 +2,12 @@ import api from './api'
 
 // Login
 export async function login(email, password) {
-  const response = await api.post('/auth/login', { email, password })
+  const response = await api.post('/api/auth/login', { email, password })
   return response.data
 }
 
 // Cadastro
 export async function register(name, email, password) {
-  const response = await api.post('/auth/register', { name, email, password })
+  const response = await api.post('/api/auth/register', { name, email, password })
   return response.data
 }
