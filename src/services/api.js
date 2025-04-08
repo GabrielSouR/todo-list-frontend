a40a6282ccacf883b387cabd082596b5f0576b98
Gabriel <d2020025314@unifei.edu.br>
2025-04-08 19:47:46 -0300

    Corrige URL de registro para API em produção

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Substitua pela URL da sua API backend
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000 // Tempo limite em milissegundos
 })
 
